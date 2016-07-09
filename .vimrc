71d4946286210e483061196bb9dcd0c709d3b07b
Abhishek Deep Nigam <adn5327@gmail.com>
2016-07-08 19:55:13 -0500

    adding an install script and upgrading the dotfiles

--- .vimrc
+++ .vimrc
@@ -9,13 +9,30 @@ set expandtab
 
 "autocmd Filetype python setlocal noexpandtab tabstop=4 shiftwidth=4
 
-
 "remap jk as the Esc option in insert mode
 inoremap jk <Esc>
 
+"remap // to search for visually selected text
+vnoremap // "zy/<C-R>"<CR>
+
+"remap keys for tab navigation
+nnoremap <C-n> :tabnew<CR>
+nnoremap tn :tabnext<CR>
+nnoremap tp :tabprevious<CR>
+
+"remap keys for easier split navigation
+nnoremap <C-j> <C-W><C-J>
+nnoremap <C-k> <C-W><C-K>
+nnoremap <C-l> <C-W><C-L>
+nnoremap <C-h> <C-W><C-H>
+
+"make splits open to right and bottom
+set splitbelow
+set splitright
+
 "display relative path in statusline, and always display it
-set statusline=%f
-set statusline+=%m
+"set statusline=%f
+"set statusline+=%m
 set laststatus=2
 
 set background=dark
@@ -24,3 +41,5 @@ colorscheme solarized
 set mouse=a
 "need this to get mouse to work properly from within tmux
 set ttymouse=xterm2
+
+execute pathogen#infect()
---
+++ install.sh
@@ -0,0 +1,32 @@
+# move old config files to .old
+mv ~/.vimrc ~/.vimrc.old
+mv ~/.gitconfig ~/.gitconfig.old
+mv ~/.tmux.conf ~/.tmux.conf.old
+mv ~/.zshrc ~/.zshrc.old
+
+# get oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# get pathogen for vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# get some plugins for vim
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+
+# setup solarized colorscheme
+git clone https://github.com/altercation/vim-colors-solarized.git ~/vim-colors-solarized
+mkdir -p ~/.vim/colors
+mv ~/vim-colors-solarized/colors/solarized.vim ~/.vim/colors
+rm -rf ~/vim-colors-solarized
+ 
+# TODO make sure tmux is installed -- brew/yum/etc
+# get tpm for tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# TODO make it such that it doesn't rely on dotfiles being in the home dir
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
